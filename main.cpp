cb3a50e2029222558af8c7328dc9d9bd0133b63b
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2024-01-13 13:25:13 +0100

    Losowanie pozycji jedzenia

--- main.cpp
+++ main.cpp
@@ -11,13 +11,14 @@ int cellCount = 25;
 
 class Food{
 public:
-    Vector2 position = {5, 6};
+    Vector2 position;
     Texture2D texture;
     Food()
     {
         Image image = LoadImage("Graphics/food.png");
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
+        position = GenerateRandomPos();
     }
     ~Food()
     {
@@ -26,6 +27,13 @@ public:
     void Draw()
     {
         DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+    Vector2 GenerateRandomPos()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x,y};
+
     }
 
 };
