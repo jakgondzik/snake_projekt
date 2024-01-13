87c442aef3ea77c2a36c071e7c534493a8606b37
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2024-01-13 14:27:42 +0100

    Przejście na klasę Game, losowanie pozycji jedzenia po pobraniu

--- main.cpp
+++ main.cpp
@@ -45,7 +45,10 @@ public :
     }
 };
 
-class Food{
+class Food
+{
+
+
 public:
     Vector2 position;
     Texture2D texture;
@@ -73,23 +76,65 @@ public:
     }
 
 };
+
+class Game
+{
+public: 
+    Snake snake = Snake();
+    Food food = Food();
+
+    void Draw()
+    {
+        food.Draw();
+        snake.Draw();
+    }
+    void Update()
+    {
+        snake.Update();
+        CheckCollisionWithFood();
+    }
+    void CheckCollisionWithFood()
+    {
+        if(Vector2Equals(snake.body[0], food.position))
+        {
+            food.position = food.GenerateRandomPos();
+        }
+    }
+};
 int main () {
     cout << "Starting the game \n";
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Project");
     SetTargetFPS(60);
-    Snake snake = Snake();
-    Food food = Food();
+
+    Game game = Game();
+
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
         if(eventTriggered(0.2))
         {
-                    snake.Update();
+                    game.Update();
+        }
+
+        if(IsKeyPressed(KEY_UP) && game.snake.direction.y != 1)
+        {
+            game.snake.direction = {0, -1};
+        }
+        if(IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1)
+        {
+            game.snake.direction = {0, 1};
+        }
+        if(IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1)
+        {
+            game.snake.direction = {-1, 0};
+        }
+        if(IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1)
+        {
+            game.snake.direction = {1, 0};
         }
 
         ClearBackground(green);
-        food.Draw();
-        snake.Draw();
+        game.Draw();
         EndDrawing();
     }
     CloseWindow();
