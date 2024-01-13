4aeb983baa415f1891da61942e66451fb4309c47
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2024-01-13 12:43:34 +0100

    Pozbycie się szablonu, dodanie tła

--- main.cpp
+++ main.cpp
@@ -3,41 +3,18 @@
 
 using namespace std;
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 42, 255};
 int main () {
-
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
-
-    cout << "Hello World" << endl;
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    cout << "Starting the game \n";
+    InitWindow(750, 750, "Snake Project");
     SetTargetFPS(60);
-
-    while (WindowShouldClose() == false){
+    while(WindowShouldClose() == false)
+    {
         BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+        ClearBackground(green);
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
